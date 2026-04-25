574b7506f771861d9a1c20fd3a277d4fcaf1df78
Byterishab <rishabsonak@gmail.com>
2026-04-26 00:18:18 +0530

    updated port injection allowance

--- app.py
+++ app.py
@@ -94,4 +94,6 @@ def api_roast():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
